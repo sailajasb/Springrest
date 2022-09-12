b733c936e0d2cbd2ac82f41d198bc1479832ef58
Sailaja.Sanagala <sailaja.sanagala@brillio.com>
2022-09-12 15:42:58 +0530

    First Commit

---
+++ src/main/java/com/brillio/springrest/controller/MyController.java
@@ -0,0 +1,17 @@
+package com.brillio.springrest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@GetMapping("/home")
+	public String home() {
+		
+		return "Welcome to my App";
+	}
+	
+	
+	
+}
